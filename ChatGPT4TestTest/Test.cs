196cec4169f5356bb6df66533996dc98e61929a0
Anna Drobot <adrobo@softserveinc.com>
2023-07-14 17:14:53 +0300

    UC#7 Create unit tests

--- ChatGPT4TestTest/Test.cs
+++ ChatGPT4TestTest/Test.cs
@@ -1,11 +1,60 @@
+using ChatGPT4TestModel;
+
 namespace ChatGPT4TestTest
 {
     public class Test
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly StudentConverter _converter;
 
+        public Test()
+        {
+            _converter = new StudentConverter();
+        }
+
+        [Fact]
+        public void HighAchieverTest()
+        {
+            var students = new List<Student> { new Student { Age = 21, Grade = 91 } };
+            var result = _converter.ConvertStudents(students);
+            Assert.True(result[0].HonorRoll);
+        }
+
+        [Fact]
+        public void ExceptionalYoungHighAchieverTest()
+        {
+            var students = new List<Student> { new Student { Age = 20, Grade = 91 } };
+            var result = _converter.ConvertStudents(students);
+            Assert.True(result[0].Exceptional);
+        }
+
+        [Fact]
+        public void PassedStudentTest()
+        {
+            var students = new List<Student> { new Student { Age = 22, Grade = 80 } };
+            var result = _converter.ConvertStudents(students);
+            Assert.True(result[0].Passed);
+        }
+
+        [Fact]
+        public void FailedStudentTest()
+        {
+            var students = new List<Student> { new Student { Age = 22, Grade = 70 } };
+            var result = _converter.ConvertStudents(students);
+            Assert.False(result[0].Passed);
+        }
+
+        [Fact]
+        public void EmptyArrayTest()
+        {
+            var students = new List<Student>();
+            var result = _converter.ConvertStudents(students);
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void NotArrayTest()
+        {
+            Assert.Throws<ArgumentNullException>(() => _converter.ConvertStudents(null));
         }
     }
 }
